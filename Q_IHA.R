15c03633fbfecc110024975474e0eb5301bd5f04
jo-dzian <dziankowska@gmail.com>
2019-03-29 19:48:12 +0100

    29.03.2019 p2

--- Q_IHA.R
+++ Q_IHA.R
@@ -22,22 +22,6 @@ Q1 <- subset(Q, Q$Date >= "2004-1-1")
 
 #reach_wyspy - ta warstwa zawiera dane o subbasinach i outletach na których będę pracować (nie pracuje na 2160 subb tylko na ok. 44)
 Island_reach <- data.frame( read.csv("reaches_island.csv"))
-#Island_subbasin$OBJECTID
 
-
-reaches <- as.numeric( Island_reach$OBJECTID)
-#Q2 <- Q1 %>% select(reaches(ends_with("1411"))
-#Q2 <- Q1 %>% select(reaches) #DZIALA
-
-
-Q3 <- Q1 %>% select(Date, c(ends_with ("1036"), ends_with ("1038"))) #ok
-
-Q3 <- Q1 %>% select(Date, c(Q1, ends_with ("1036")))
-
-Q3 <- Q1 %>% select(Date, matches('1036|1038'))
-Q3 <- Q1 %>% select(Date, matches("1036|1038|1077|1139|1213|1258|1302|1363|1387|1411|1451|1480|1580|1654|1697|1716|1725|1751|1779|1858|1910|1931|2054"))
-
-#Q2 <- select(Q1, ends_with("1036"))#DZIALA
-
-Q2 <- select(Q1, ends_with("1411"))
-    
+#zawezam dane do interesujacych mnie reachow
+Q2 <- Q1 %>% select(Date, matches("1036|1038|1077|1139|1213|1258|1302|1363|1387|1411|1451|1480|1580|1654|1697|1716|1725|1751|1779|1858|1910|1931|2054"))
