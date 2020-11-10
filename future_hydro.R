c74d618a06d09c549132e8adf2323c7f5cbf2977
jo-dzian <dziankowska@gmail.com>
2020-11-10 13:34:20 +0100

    10.11.2020 v1

--- future_hydro.R
+++ future_hydro.R
@@ -10,8 +10,6 @@ subbasins_nr <- data.frame(rbind("910","950", "1012", "1087", "1134","1240","126
 "1565","1601","1629", "1727","1746","1875"))
 colnames(subbasins_nr)<- c("RCH")
 
-
-
 # 4.5 NF
 
 mydir_45_NF = "D:/Ptaki_hydro/Obliczenia/4TU/future_csv/4.5_NF"
@@ -24,11 +22,38 @@ dat_csv_45_NF <- list.files(path=mydir_45_NF, full.names = TRUE) %>%
 
 
 test <- for (i in myfiles_45_NF[-1]) {
-  out <- read.csv(i, header = TRUE, sep = ",")[, c("subbasin" ,"date", "flow")]
-  out<-  subset(i, "subbasin" == subbasins_nr )
+  into <- read.csv(i, header = TRUE, sep = ",")[, c("subbasin" ,"date", "flow")]
+  out <-  subset(into, "subbasin" == subbasins_nr )
+  #out2 <- 
+  print(out)
   #colnames(out) <- c("date", basename(myfiles_45_NF[i]))
-  result <- merge(out, by = "date")
+  #result <- merge(out, by = "date")
 }
+
+
+test <- for (i in myfiles_45_NF) {
+  in_data <- read.csv(i, header = TRUE, sep = ",")[, c("subbasin" ,"date", "flow")]
+  in_data2 <- in_data[ in_data$subbasin %in% subbasins_nr$RCH, ]
+  in_data2$date2 <- as.POSIXct(in_data2$date, format="%Y-%m-%d")
+  month <- as.integer(format(in_data2$date2, '%m'))
+  day <- as.integer(format(in_data2$date2, '%d'))
+  in_data3 <- filter(in_data2, month == 4 & day >= 11 | month == 5 | month == 6 & day <= 10)
+  in_data4 <- split(in_data3, in_data3$subbasin)
+  write.csv(in_data4, "D:/Ptaki_hydro/Obliczenia/4TU/future_csv/4.5_NF/VP_bh.gull")
+}
+
+test_fun <- function(x){
+  in_data <- read.csv(x, header = TRUE, sep = ",")
+  in_data2 <- in_data[ in_data$subbasin %in% subbasins_nr$RCH, ]
+  in_data2$date2 <- as.POSIXct(in_data2$date, format="%Y-%m-%d")
+  month <- as.integer(format(in_data2$date2, '%m'))
+  day <- as.integer(format(in_data2$date2, '%d'))
+  in_data3 <- filter(in_data2, month == 4 & day >= 11 | month == 5 | month == 6 & day <= 10)
+  in_data4 <- split(in_data3, in_data3$subbasin)
+  }
+
+
+test <- test_fun("D:/Ptaki_hydro/Obliczenia/4TU/future_csv/4.5_NF/rcp45_cm01_2024_2050_reach.csv")
 
 test1 <- read.csv("D:/Ptaki_hydro/Obliczenia/4TU/future_csv/4.5_NF/rcp45_cm01_2024_2050_reach.csv",
                   header = TRUE, sep = ",")
@@ -36,6 +61,15 @@ test1 <- read.csv("D:/Ptaki_hydro/Obliczenia/4TU/future_csv/4.5_NF/rcp45_cm01_20
 test2 <- test1[ test1$subbasin %in% channels_for_calibration$Subbasin, ]
 
 test2 <- test1[ test1$subbasin %in% subbasins_nr$RCH, ]
+
+#test2 <- as.Date(test2$date, format="%Y-%m-%d")
+test2$date2 <- as.POSIXct(test2$date, format="%Y-%m-%d")
+
+month <- as.integer(format(test2$date2, '%m'))
+day <- as.integer(format(test2$date2, '%d'))
+result <- filter(test2, month == 4 & day >= 11 | month == 5 | month == 6 & day <= 10)
+result2 <- split(result, result$subbasin)
+
 
 # 4.5 FF
 
